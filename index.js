254fe07c28486d3aeb79dcbf03dac212b5a1eb61
Ignazio Gaudio <i_gaudio@hotmail.com>
2023-05-17 22:06:44 +1000

    added input data and basic functions

--- index.js
+++ index.js
@@ -6,11 +6,53 @@ const path = require('path')
 let dir = '../Dist'
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        section: "Please give Github username"
+      },
+      {
+        type: "input",
+        name: "title",
+        section: "Please give project title"
+      },
+      {
+        type: "input",
+        name: "description",
+        section: "Please give description"
+      },
+      {
+        type: "input",
+        name: "contents",
+        section: "Please give Table of Contents "
+      },
+      {
+        type: "input",
+        name: "instructions",
+        section: "Please give installation instructions"
+      },
+      {
+        type: "input",
+        name: "usageinfo",
+        section: "Please give usage information"
+      },
+      {
+        type: "input",
+        name: "contributions",
+        section: "Please give contributors"
+      },
+      {
+        type: "input",
+        name: "test",
+        section: "Please give any test instructions"
+      },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    return fs.writeFileSync(path.join(fileName), data);
 }
 
 // TODO: Create a function to initialize app
