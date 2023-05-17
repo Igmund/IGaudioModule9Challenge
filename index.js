d2fd529e21d515fb79f4eae97565477b1dfccfa2
Ignazio Gaudio <i_gaudio@hotmail.com>
2023-05-17 21:42:38 +1000

    added functions to index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const gm = require('../utils/generateMarkdown')
+const path = require('path')
+let dir = '../Dist'
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        writeToFile("README.md", gm({...answers}));
+    })
+}
 
 // Function call to initialize app
 init();
